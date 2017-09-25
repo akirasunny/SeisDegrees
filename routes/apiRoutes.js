3b11aa7f328ba68476e34ca981109b79d289369e
Stephen Woo <stephenwoo95@gmail.com>
2017-09-25 10:32:18 -0700

    merging

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -13,15 +13,6 @@ var chatController = require("../controllers/chatController");
 
 var router = new express.Router();
 
-/*// Get all articles (or optionally a specific article with an id)
-router.get("/articles/:id?", articlesController.index);
-// Create a new article using data passed in req.body
-router.post("/articles", articlesController.create);
-// Update an existing article with a speicified id param, using data in req.body
-router.patch("/articles/:id", articlesController.update);
-// Delete a specific article using the id in req.params.id
-router.delete("/articles/:id", articlesController.destroy);*/
-
 // USER
 
 // Create a new User
