574ad36075f5be41a925e8f3ead72cd6495b1321
SueAkira <Akira@SueAkiradeMacBook-Air.local>
2017-09-27 12:06:25 -0700

    photo upload finished

--- app/components/Post.js
+++ app/components/Post.js
@@ -2,43 +2,66 @@ import React, { Component } from 'react'
 import {
   Form, TextArea, Container, Divider, Dropdown, Grid, Header, Icon, Image, List, Menu, Segment, Button,
 } from 'semantic-ui-react';
-import ReactCoreImageUpload  from 'react-core-image-upload';
+import Images from "./Images";
 
 class Post extends Component {
 	constructor() {
 		super();
-		this.state = {
-			src: 'http://img1.vued.vanthink.cn/vued0a233185b6027244f9d43e653227439a.png'
-		};
-		this.imageuploaded = this.imageuploaded.bind(this);
+		this.saveImage = this.saveImage.bind(this);
+		this.saveLocation = this.saveLocation.bind(this);
+		this.saveDate = this.saveDate.bind(this);
+		this.handleInput = this.handleInput.bind(this);
 	}
-	imageuploaded(res) {
-		if (res.errcode == 0) {
-			this.setState({
-				src: res.data.src,
-			});
-		}
-	}
-	componentWillMount() {
 
+	saveImage(src) {
+		this.setState({ img: src });
+	}
+
+	saveLocation(loc) {
+		this.setState({ location: loc });
+	}
+
+	saveDate(date) {
+		this.setState({ date: date });
+	}
+
+	handleInput(event) {
+		var obj = {};
+		obj[event.target.id] = event.target.value;
+		console.log(obj);
+		this.setState(obj);
+	}
+
+	componentWillMount() {
+		this.setState({ id: this.props.id, username: this.props.username })
 	}
 
 	render() {
 		return (
 		<Container style={{ marginTop: 20 }}>
 			<Form>
-			<Form.Input placeholder="Title" id="title"/>
-			<TextArea placeholder="What's new?" id="body" style={{ minHeight: 100 }} />
+			<Form.Input placeholder="Title" id="title" onChange={this.handleInput}/>
+			<TextArea placeholder="What's new?" id="body" onChange={this.handleInput} style={{ minHeight: 100 }} />
 			</Form>
 			<Container style={{ padding: 10 }}>
 
-			<Icon name="photo" size="large"/>
+			<Images id={this.props.id} saveImage={this.saveImage}/>
 
 			<Icon name="marker" size="large"/>
 
 			<Icon name="add user" size="large"/>
 
+			<Icon name="calendar" size="large"/>
+
 			<Button floated="right" color="grey">Post</Button>
+			</Container>
+			<Container>
+			{this.state.img && 
+				this.state.img.map((data, i) => {
+				return(
+					<Image size="small" inline shape="circular" key={i} src={"/" + data} />
+				)
+			})}
 			</Container>
 		 </Container>
 		)
