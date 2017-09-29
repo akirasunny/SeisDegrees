1bb19542813a30b0dbeb5f2aeb465471504d3b6f
SueAkira <Akira@SueAkiradeMacBook-Air.local>
2017-09-29 09:30:54 -0700

    Stylized homepage

--- app/components/Home.js
+++ app/components/Home.js
@@ -13,6 +13,7 @@ import {
   Visibility,
 } from 'semantic-ui-react';
 import { Link } from 'react-router';
+import { Carousel } from 'react-responsive-carousel';
 
 class Home extends Component {
   render() {
@@ -21,13 +22,12 @@ class Home extends Component {
       <Segment
         inverted
         textAlign='center'
-        style={{ minHeight: 500, padding: '1em 0em' }}
+        style={{ padding: '1em 0em' }}
         vertical
       >
-        <Container text>
           <Header
             as='h1'
-            content='6 Degrees'
+            content='SeisDegrees'
             inverted
             style={{ fontSize: '4em', fontWeight: 'normal', marginBottom: 0, marginTop: '3em' }}
           />
@@ -35,88 +35,36 @@ class Home extends Component {
             as='h2'
             content='Record your footprints.'
             inverted
-            style={{ fontSize: '1.7em', fontWeight: 'normal' }}
+            style={{ fontSize: '1.7em', fontWeight: 'normal', marginBottom: '3em'}}
           />
-          <Button primary size='huge'>
-            Get Started
-            <Icon name='right arrow' />
-          </Button>
-        </Container>
+          <Carousel showArrows={true} autoPlay interval={2000}>
+            <div>
+                <img src="../../public/assets/Carousel/01.jpg" />
+                <p className="legend">Pike Market @ Seattle</p>
+            </div>
+            <div>
+                <img src="../../public/assets/Carousel/02.jpg" />
+                <p className="legend">Avenue @ Seattle</p>
+            </div>
+            <div>
+                <img src="../../public/assets/Carousel/03.jpg" />
+                <p className="legend">Bleeding Heart @ Central Park, NYC</p>
+            </div>
+            <div>
+                <img src="../../public/assets/Carousel/04.jpg" />
+                <p className="legend">Malus Spectabilis @ Ithaca, NY</p>
+            </div>
+            <div>
+                <img src="../../public/assets/Carousel/05.jpg" />
+                <p className="legend">Infinite Mirror - Yayoi Kusama</p>
+            </div>
+            <div>
+                <img src="../../public/assets/Carousel/06.jpg" />
+                <p className="legend">Cat's gem-like eye</p>
+            </div>
+        </Carousel>
       </Segment>
 
-      <Segment style={{ padding: '8em 0em' }} vertical>
-        <Grid container stackable verticalAlign='middle'>
-          <Grid.Row>
-            <Grid.Column width={8}>
-              <Header as='h3' style={{ fontSize: '2em' }}>A Header</Header>
-              <p style={{ fontSize: '1.33em' }}>
-                Some texts
-              </p>
-              <Header as='h3' style={{ fontSize: '2em' }}>Another Header</Header>
-              <p style={{ fontSize: '1.33em' }}>
-                More texts
-              </p>
-            </Grid.Column>
-            <Grid.Column floated='right' width={6}>
-              <Image
-                bordered
-                shape="circular"
-                size='large'
-                src='#'
-                alt="An image"
-              />
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column textAlign='center'>
-              <Button size='huge'>Check Them Out</Button>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-      <Segment style={{ padding: '0em' }} vertical>
-        <Grid celled='internally' columns='equal' stackable>
-          <Grid.Row textAlign='center'>
-            <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-              <Header as='h3' style={{ fontSize: '2em' }}>"What a Company"</Header>
-              <p style={{ fontSize: '1.33em' }}>That is what they all say about us</p>
-            </Grid.Column>
-            <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-              <Header as='h3' style={{ fontSize: '2em' }}>"I shouldn't have gone with their competitor."</Header>
-              <p style={{ fontSize: '1.33em' }}>
-                <Image avatar src='#' alt="Avatar" />
-                <b>Nan</b> Chief Fun Officer Acme Toys
-              </p>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-      <Segment style={{ padding: '8em 0em' }} vertical>
-        <Container text>
-          <Header as='h3' style={{ fontSize: '2em' }}>Breaking The Grid, Grabs Your Attention</Header>
-          <p style={{ fontSize: '1.33em' }}>
-            Instead of focusing on content creation and hard work, we have learned how to master the art of doing
-            nothing by providing massive amounts of whitespace and generic content that can seem massive, monolithic
-            and worth your attention.
-          </p>
-          <Button as='a' size='large'>Read More</Button>
-          <Divider
-            as='h4'
-            className='header'
-            horizontal
-            style={{ margin: '3em 0em', textTransform: 'uppercase' }}
-          >
-            <a href='#'>Case Studies</a>
-          </Divider>
-          <Header as='h3' style={{ fontSize: '2em' }}>Did We Tell You About Our Bananas?</Header>
-          <p style={{ fontSize: '1.33em' }}>
-            Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but its really
-            true.
-            It took years of gene splicing and combinatory DNA research, but our bananas can really dance.
-          </p>
-          <Button as='a' size='large'>I'm Still Quite Interested</Button>
-        </Container>
-      </Segment>
         <Segment
           inverted
           style={{ margin: '5em 0em 0em', padding: '5em 0em' }}
@@ -160,7 +108,6 @@ class Home extends Component {
               </Grid.Row>
             </Grid>
             <Divider inverted section />
-            <Image src='/logo.png' centered size='mini' />
             <List horizontal inverted divided link>
               <List.Item as='a' href='https://github.com/akirasunny/diary'>GitHub Repository</List.Item>
               <List.Item as='a' href='#'>Terms and Conditions</List.Item>
